7e4436624bb38746e848b341bc5c6b5428410970
ndirituart <ndiritupatience002@gmail.com>
2026-08-31 12:22:28 +0300

    Fixing issues with Bootstrap-icons installation in Angular.json,Tscongfig.json and Tsconfig.app.json

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+// Change to have the ANY in err
 bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+  .catch((err: unknown) => console.error(err));
