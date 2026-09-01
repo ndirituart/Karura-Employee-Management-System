c9ff070e05b787d92ecc17257bf94158dae52046
ndirituart <ndiritupatience002@gmail.com>
2026-09-01 11:01:06 +0300

    Updating in the default home page, struggling with routing links to the different pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //default page for admins
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadComponent: () => import('./pages/login/login').then(m => m.Login) },//refuses component: LoginComponent
+  { path: 'dashboard', component: Dashboard },
+  { path: 'project-employee', loadComponent: () => import('./pages/project-employee/project-employee').then(m => m.ProjectEmployee) },
+  { path: 'project', loadComponent: () => import('./pages/project/project').then(m => m.Project) },
+
+  // Add other page routes here
+];
