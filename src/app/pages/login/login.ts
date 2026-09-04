5bb0503f970e0c851c412a358f4c21b4d6beaa46
ndirituart <ndiritupatience002@gmail.com>
2026-09-04 11:40:47 +0300

    Creating login object and function. Declared values for admins and verification as well as error handling in case of wrong credentials

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,25 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './login.css',
 })
 export class Login {
+  //1. Create an object that handles the form values for email and password.
+  // This object will be used to bind the form inputs to the component's properties.
   loginObj: any =
   {
   email: '', //instead of username for security and authentication
   password: ''
+    }
+  //2. Create a function that handles the form submission when the user clicks the login button.
+  router = inject(Router);
+
+  onLogin() {
+    //3. Temporary credentials for testing purposes. In a real application, you would validate the credentials against a backend service.
+    if (this.loginObj.email === 'ndiritupatience002@gmail.com' && this.loginObj.password === '123456abj') {
+      //4. Navigate to the dashboard page upon successful login.
+      this.router.navigate(['/dashboard']);
+    }
+    else {
+      //5. Display an error message if the credentials are invalid.
+      alert('Invalid email or password. Please try again.');
+    }
   }
 }
